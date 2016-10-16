b17a879d43affce714e58c34a9f42860864a2fa8
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-10-16 21:39:33 +0800

    Fix env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export ROS_WS=~/catkin_ws
-. $ROS_WS/devel/setup.bash
+ROS_WS=~/catkin_ws
+. ${ROS_WS}/devel/setup.bash
 . /opt/ros/kinetic/setup.bash
 
 export PATH=$ROS_ROOT/bin:$PATH
@@ -9,5 +9,7 @@ export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
 export ROS_HOSTNAME=$HOSTNAME.local
 export ROS_IP=`hostname -I`
+
+. ${ROS_WS}/devel/setup.sh
 
 exec "$@"
