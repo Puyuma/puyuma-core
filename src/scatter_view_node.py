10c79c1e368911c53921886ffce2e5360c4a9ca4
ponsheng <a7061120@gmail.com>
2016-12-26 13:43:28 +0800

    display different color point

--- src/scatter_view_node.py
+++ src/scatter_view_node.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
 import pyqtgraph as pg
 import numpy as np
-from scatter_view.msg import segment
-from scatter_view.msg import segmentArray
+from xenobot.msg import segment
+from xenobot.msg import segmentArray
 from pyqtgraph.Qt import QtGui
 from PyQt4.QtCore import QObject, pyqtSignal
 import signal
@@ -13,8 +12,10 @@ import sys
 
 plotWidget = None
 app = None
-d = []
-phi = []
+d_y = []
+phi_y = []
+d_w = []
+phi_w = []
 foo = None
 
 class Foo(QObject):
@@ -25,21 +26,33 @@ class Foo(QObject):
 		self.trigger.emit()
 	def handler(self):
 		plotWidget.clear()
-		plotWidget.plot(d,phi,pen=None,symbol='x')
+		plotWidget.plot(d_w,phi_w,pen=None,symbol='x',symbolBrush=pg.mkBrush('r'))
+		plotWidget.plot(d_y,phi_y,pen=None,symbol='x',symnolbrush=pg.mkBrush('w'))
 
 def sig_INT_handler(signal, frame):
 	print('You pressed Ctrl+C!')
 	app.quit()
 
 def data_cb(msg):
-	global d,phi
+	global d_y,d_w,phi_y,phi_w
+
 	size = len(msg.segments)
 	rospy.loginfo("get %d data",size)
-	d = []
-	phi = []
+	d_y = []
+	d_w = []
+	phi_y = []
+	phi_w = []
+
 	for seg in msg.segments:
-		d.append(seg.d)
-		phi.append(seg.phi)
+		if(seg.color == 1): #yellow
+			d_y.append(seg.d)
+			phi_y.append(seg.phi)
+		elif (seg.color == 0): #white
+			d_w.append(seg.d)
+			phi_w.append(seg.phi)
+		else:
+			rospy.loginfo("invalid color code")
+
 	foo.e()
 
 def main():
@@ -50,7 +63,7 @@ def main():
 
 	app = QtGui.QApplication([])
 	foo = Foo()
-	rospy.init_node('talker',anonymous=True)
+	rospy.init_node('scatter_view_node',anonymous=True)
 
 	topic_name = "/xenobot/segment_data"
 	rospy.Subscriber(topic_name, segmentArray, data_cb)
