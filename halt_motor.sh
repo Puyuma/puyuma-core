55451d4393b06d2fa8c9d0b776a82307b1cbbc89
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-01-10 15:40:15 +0800

    Create script to halt the motor

---
+++ halt_motor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+/usr/local/bin/gpio mode 0 out
+/usr/local/bin/gpio mode 1 out
+/usr/local/bin/gpio mode 4 out
+/usr/local/bin/gpio mode 26 out
+/usr/local/bin/gpio write 0 0
+/usr/local/bin/gpio write 1 0
+/usr/local/bin/gpio write 4 0
+/usr/local/bin/gpio write 26 0
