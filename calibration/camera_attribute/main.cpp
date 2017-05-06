8cfacc46b71bda092e9fb62386e2616a2bc5cf53
ponsheng <a7061120@gmail.com>
2017-05-06 15:18:57 +0800

    Camera attribute test now available
    1.Launch by "roslaunch xenobot camera_attribute.launch veh:=VEH"
    2.Watch image using visual tools
    3.Set camera option using: rosservice call /set_att '{name: 0,value: 20}'
    Add valgrind option in launch file
    Though valgrind is not valid on rpi3 now

--- calibration/camera_attribute/main.cpp
+++ calibration/camera_attribute/main.cpp
@@ -4,17 +4,69 @@
 #include <cv_bridge/cv_bridge.h>
 #include <camera.hpp>
 
+#include <xenobot/SetAtt.h>
+
 using namespace cv;
 
 raspicam::RaspiCam_Cv camera;
 
+//OpenCV only for cameras attribute
+int atts[] = {
+	CV_CAP_PROP_BRIGHTNESS, //Brightness of the image
+	CV_CAP_PROP_CONTRAST, //Contrast of the image
+	CV_CAP_PROP_SATURATION, //Saturation of the image
+	CV_CAP_PROP_HUE, //Hue of the image (only for cameras).
+	CV_CAP_PROP_GAIN, //Gain of the image (only for cameras).
+	CV_CAP_PROP_EXPOSURE, //Exposure (only for cameras).
+	CV_CAP_PROP_CONVERT_RGB //Boolean flags indicating whether images should be converted to RGB.
+};
+std::string names[] = {
+	"CV_CAP_PROP_BRIGHTNESS",
+    "CV_CAP_PROP_CONTRAST",
+    "CV_CAP_PROP_SATURATION",
+    "CV_CAP_PROP_HUE",
+    "CV_CAP_PROP_GAIN",
+    "CV_CAP_PROP_EXPOSURE",
+    "CV_CAP_PROP_CONVERT_RGB"
+};
+bool set_att(xenobot::SetAtt::Request &req,xenobot::SetAtt::Response &res)
+{
+	ROS_INFO("set att");
+	std::string result= "\n";
+
+	if(req.name==0) {
+
+		int arr_size = (sizeof(atts)/sizeof(int));
+		// print all  information
+		for(int i=0; i < arr_size; i++) {
+			result += std::to_string(i+1);
+			result += " " + names[i];
+			result += ": ";
+			result += std::to_string((int)camera.get(atts[i]));
+			result += "\n";
+
+		}
+	} else if(req.name <= sizeof(atts)) {
+		int i = req.name-1;
+		if(camera.set(atts[i],req.value)) {
+			result += "Set " + names[i] + " " + std::to_string(req.value);
+		} else {
+			result += "Failed";
+		}
+		ROS_INFO("%s",result.c_str());
+	}
+
+	res.msg = result;
+
+	return true;
+}
 
 int main(int argc, char* argv[])
 {
 	ros::init(argc, argv, "camera_attribute");
 	ros::NodeHandle node;
-	ros::Rate loop_rate(20);
-	//ros::ServiceServer srv = node.advertiseService("", save_yaml_parameter);;
+	ros::Rate loop_rate(5);
+	ros::ServiceServer srv = node.advertiseService("set_att", set_att);;
 	cv::Mat frame, bgr_frame;
 
 	if(!camera_setup(camera)) {
@@ -22,7 +74,6 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
-	namedWindow(win_name);
 	ros::Publisher raw_image_publisher = node.advertise<sensor_msgs::Image>("raw_image", 1);
 
 	while(ros::ok()) {
@@ -31,6 +82,7 @@ int main(int argc, char* argv[])
 		sensor_msgs::ImagePtr img_msg =
 			cv_bridge::CvImage(std_msgs::Header(), "rgb8", frame).toImageMsg();
 		raw_image_publisher.publish(img_msg);
+		ros::spinOnce();
 		loop_rate.sleep();
 	}
 	return 0;
